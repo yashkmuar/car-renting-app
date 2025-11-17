de2272004af8629063665d2336486a7989d711c4
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-17 23:38:14 +0530

    object creation and binding with the form username and password is completed

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  loginObj: any = {
+    userName:'',
+    password:''
+  }
 
 }
