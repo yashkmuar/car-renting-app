a617fd0b66a8c3fffb2247e491350f7a7b91acfb
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-17 23:12:29 +0530

    login route and layout route added, in layout route we also have child routes which again shows us navigation bar and some other things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,32 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Vehicles } from './pages/vehicles/vehicles';
 
 export const routes: Routes = [
     {
         path:'',
         redirectTo:'login',
         pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:Login
+    },
+    {
+        path:'',
+        component:Layout,
+        children:[
+            {
+                path:'dashboard',
+                component:Dashboard
+            },
+            {
+                path:'vehicles',
+                component:Vehicles
+            }
+        ]
     }
+
 ];
