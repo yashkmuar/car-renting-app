2849688e8b23dc95bb94287059a799ef3f779544
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-22 23:29:24 +0530

    car model property initialization done

--- src/app/model/car.ts
+++ src/app/model/car.ts
@@ -3,8 +3,19 @@ export class CarModel {
     carImage: string;
     brand: string;
     model: string;
-    year: string;
+    year: number;
     color: string;
     dailyRate: number;
     regNo: string;
+
+    constructor(){
+        this.brand='';
+        this.carId=0;
+        this.carImage='';
+        this.model='';
+        this.year=0;
+        this.color='';
+        this.dailyRate=0;
+        this.regNo='';
+    }
 }
