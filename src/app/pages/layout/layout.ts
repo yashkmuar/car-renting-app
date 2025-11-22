46135241692101a4eaab16782b1d3bf8bb1d074d
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-22 22:28:24 +0530

    in layout component, vehicle routing established

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
