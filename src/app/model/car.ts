06213c54fc82e589f417cc46667885641c90f352
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-22 23:27:39 +0530

    car model class created

---
+++ src/app/model/car.ts
@@ -0,0 +1,10 @@
+export class CarModel {
+    carId: number;
+    carImage: string;
+    brand: string;
+    model: string;
+    year: string;
+    color: string;
+    dailyRate: number;
+    regNo: string;
+}
