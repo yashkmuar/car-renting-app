2964ca5a362ecf37df32d0796ce03a3e1737f04a
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-22 23:33:41 +0530

    carmodel bind with vehicles.ts file and use of ngModel in html to bind with ts file, for strong typing

--- src/app/pages/vehicles/vehicles.ts
+++ src/app/pages/vehicles/vehicles.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CarModel } from '../../model/car';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './vehicles.html',
   styleUrl: './vehicles.css',
 })
 export class Vehicles {
+  newCarObj:CarModel;
+
+  constructor(){
+    this.newCarObj = new CarModel();
+  }
+
 
 }
